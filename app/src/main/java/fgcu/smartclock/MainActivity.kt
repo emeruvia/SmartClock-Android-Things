6dde491a60355181d7481f1dc508dad17ed1e224
jeffrey <jeffreyfleurent@outlook.com>
2018-10-14 17:09:20 -0400

    Added change to make awareness work in MainActivity

--- app/src/main/java/fgcu/smartclock/MainActivity.kt
+++ app/src/main/java/fgcu/smartclock/MainActivity.kt
@@ -1,7 +1,15 @@
 package fgcu.smartclock
 
 import android.app.Activity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.gms.awareness.Awareness
+import com.google.android.gms.common.api.GoogleApiClient
 
 /**
  * Skeleton of an Android Things activity.
@@ -23,10 +31,49 @@ import android.os.Bundle
  * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
  *
  */
-class MainActivity : Activity() {
+class MainActivity : Activity(), GoogleApiClient.ConnectionCallbacks {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+  var client : GoogleApiClient? = null
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    client = GoogleApiClient.Builder(this)
+        .addApi(Awareness.API)
+        .addConnectionCallbacks(this)
+        .build()
+
+    client?.connect()
+    if (client?.isConnected as Boolean) {
+      Log.d("MainActivity", "Connection Sucessful")
+
     }
+
+  }
+
+  override fun onConnectionSuspended(p0: Int) {
+    Log.d("MainActivity", "Entered onConnectionSuspended")
+
+  }
+
+  override fun onConnected(p0: Bundle?) {
+    Log.d("MainActivity", "Entered onConnected")
+
+    Log.d("MainActivity", "Tried getWeather")
+    Awareness.SnapshotApi.getWeather(client)
+        .setResultCallback { WeatherData ->
+          if (!WeatherData.status.isSuccess) Log.d("MainActivity", "Weather Failed") else
+            Log.d("MainActivity", "Weather : " +WeatherData.status.isSuccess)
+        }
+
+    Log.d("MainActivity", "Tried getLocation")
+    Awareness.SnapshotApi.getPlaces(client)
+        .setResultCallback { placesResult ->
+          if (!placesResult.status.isSuccess) Log.d("MainActivity", "Places Failed")
+          Log.d("MainActivity", "Places : " +placesResult.status.isSuccess)
+        }
+
+
+
+//    Log.d("MainActivity", "Places : " + string1 + " Weather : " + string2)
+  }
 }
--- app/src/main/java/fgcu/smartclock/WeatherData.kt
+++ app/src/main/java/fgcu/smartclock/WeatherData.kt
@@ -1,6 +1,7 @@
 package fgcu.smartclock
 
 import android.content.Context
+import android.os.Bundle
 import android.util.Log
 import com.google.android.gms.awareness.Awareness
 import com.google.android.gms.awareness.state.Weather
@@ -9,7 +10,7 @@ import com.google.android.gms.common.api.GoogleApiClient
 
 class WeatherData(
   val context: Context
-) {
+) : GoogleApiClient.ConnectionCallbacks {
   var address: CharSequence? = null
   var weather: Weather? = null
 
@@ -20,18 +21,30 @@ class WeatherData(
 
     client.connect()
 
-    Awareness.getSnapshotClient(context).places.addOnCompleteListener { task -> address = task.result.toString()  }
-    Log.d("WeatherData", "Places successfully loaded: "+ Awareness.getSnapshotClient(context).places.isComplete)
-//    Awareness.SnapshotApi.getPlaces(client)
-//        .setResultCallback { placesResult ->
-//          if (placesResult == null) address = null else address =
-//              placesResult.placeLikelihoods[0].place.address.toString()
-//        }
+    Awareness.SnapshotApi.getPlaces(client)
+        .setResultCallback { placesResult ->
+          if (placesResult == null) address = null else address =
+              placesResult.placeLikelihoods[0].place.address.toString()
+        }
 
     Awareness.SnapshotApi.getWeather(client)
         .setResultCallback { WeatherData ->
           if (WeatherData == null) weather = null else weather = WeatherData.weather
         }
   }
+
+
+  override fun onConnectionSuspended(p0: Int) {
+
+
+  }
+
+  override fun onConnected(p0: Bundle?) {
+    var string1 =Awareness.getSnapshotClient(context).places.result.toString()
+    var string2 = Awareness.getSnapshotClient(context).weather.result.toString()
+    Log.d("WeatherData","Plces : " + string1 + " Weather : "+ string2)
+  }
+
+
 
 }
