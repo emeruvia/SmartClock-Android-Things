e1447291190ae934ddff6f845b48295d6d2b1e69
Edgar Meruvia Garron <emeruvia@gmail.com>
2018-10-18 19:47:21 -0400

    Updated weather model to avoid crashes

--- app/src/main/java/fgcu/smartclock/models/WeatherModel.kt
+++ app/src/main/java/fgcu/smartclock/models/WeatherModel.kt
@@ -3,9 +3,13 @@ package fgcu.smartclock.models
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
-data class WeatherInfoModel(
+data class WeatherModel(
   @SerializedName("weather")
-  val weatherCondition: WeatherConditionModel,
+  val weatherCondition: List<WeatherConditionModel>,
   @SerializedName("main")
-  val temperature: TemperatureModel
+  val temperature: TemperatureModel,
+  @SerializedName("dt")
+  val date: Double,
+  @SerializedName("name")
+  val city: String
 ) : Serializable
